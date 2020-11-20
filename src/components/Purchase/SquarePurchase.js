c14a136be2fb419f30121b7fb3de1bb56890f0a7
Trenton Wesche <TrWesche@live.com>
2020-11-20 16:08:57 -0600

    Square basic end to end payment processing working

--- src/components/Purchase/SquarePurchase.js
+++ src/components/Purchase/SquarePurchase.js
@@ -3,31 +3,150 @@
 
 // https://github.com/square/react-square-payment-form
 
-import React from 'react';
+// https://square.github.io/react-square-payment-form/docs/paymentform
+
+// import { 
+//   Container
+// } from "@material-ui/core";
+// import { makeStyles } from '@material-ui/core/styles';
+
+
+import React, { useState } from 'react';
+import axios from 'axios';
 import { 
-  Container
-} from "@material-ui/core";
-import { makeStyles } from '@material-ui/core/styles';
+  SquarePaymentForm,
+  ApplePayButton,
+  CreditCardCVVInput,
+  CreditCardExpirationDateInput,
+  CreditCardNumberInput,
+  CreditCardPostalCodeInput,
+  CreditCardSubmitButton,
+  GooglePayButton,
+  MasterpassButton
+} from 'react-square-payment-form';
+import 'react-square-payment-form/lib/default.css';
 
+const APPLICATION_ID = process.env.REACT_APP_SQUARE_APP_ID;
+const LOCATION_ID = process.env.REACT_APP_SQUARE_LOC_ID;
+const BASE_URL = process.env.REACT_APP_BASE_URL || "http://localhost:5000/api";
 
-const useStyles = makeStyles((theme) => ({
-  root: {
-    display: 'flex',
-    flexWrap: 'wrap',
-  },
-  margin: {
-    margin: theme.spacing(1),
-  },
-  textField: {
-    width: '25ch',
-  },
-}));
+const SquarePurchase = () => {
+  console.log(APPLICATION_ID, LOCATION_ID);
+  const [errorMessages, setErrorMessages] = useState([]);
 
-export default function SquarePurchase() {
-  const classes = useStyles();
-  return (
-    <Container>
-      <p>Square Purchase Under Construction</p>
-    </Container>
+  function cardNonceResponseReceived(errors, nonce, cardData, buyerVerificationToken) {
+    if (errors) {
+      setErrorMessages(errors.map(error => error.message));
+      return;
+    }
+
+    setErrorMessages([]);
+
+    // alert('nonce created: ' + nonce + ', buyerVerificationToken: ' + buyerVerificationToken);
+    const result = axios.post(`${BASE_URL}/sqpay/process-payment`, {nonce: nonce, buyerVerificationToken: buyerVerificationToken});
+    console.log(result);
+  }
+
+  function createPaymentRequest() {
+    return {
+      requestShippingAddress: false,
+      requestBillingInfo: true,
+      currencyCode: 'USD',
+      countryCode: 'US',
+      total: {
+        label: 'MERCHANT NAME',
+        amount: '1',
+        pending: false,
+      },
+      lineItems: [
+        {
+          label: 'Subtotal',
+          amount: '1',
+          pending: false,
+        },
+      ],
+    };
+  }
+
+  function createVerificationDetails() {
+    return {
+      amount: '100.00',
+      currencyCode: 'USD',
+      intent: 'CHARGE',
+      billingContact: {
+        familyName: 'Smith',
+        givenName: 'John',
+        email: 'jsmith@example.com',
+        country: 'GB',
+        city: 'London',
+        addressLines: ["1235 Emperor's Gate"],
+        postalCode: 'SW7 4JA',
+        phone: '020 7946 0532',
+      },
+    };
+  }
+
+  function postalCode() {
+    const postalCode = '12345'; // your logic here
+    return postalCode;
+  }
+
+  function focusField() {
+    return 'cardNumber';
+  }
+
+  const loadingView = <div className="sq-wallet-loading"></div>;
+  const unavailableApple = (
+    <div className="sq-wallet-unavailable">Apple pay unavailable. Open safari on desktop or mobile to use.</div>
   );
-}
+  const unavailableGoogle = <div className="sq-wallet-unavailable">Google pay unavailable.</div>;
+  const unavailableMasterpass = <div className="sq-wallet-unavailable">Masterpass unavailable.</div>;
+
+  return (
+    <SquarePaymentForm
+      sandbox={true}
+      applicationId={APPLICATION_ID}
+      locationId={LOCATION_ID}
+      cardNonceResponseReceived={cardNonceResponseReceived}
+      createPaymentRequest={createPaymentRequest}
+      createVerificationDetails={createVerificationDetails}
+      postalCode={postalCode}
+      focusField={focusField}
+    >
+      <ApplePayButton loadingView={loadingView} unavailableView={unavailableApple} />
+      <GooglePayButton loadingView={loadingView} unavailableView={unavailableGoogle} />
+      <MasterpassButton loadingView={loadingView} unavailableView={unavailableMasterpass} />
+
+      <div className="sq-divider">
+        <span className="sq-divider-label">Or</span>
+        <hr className="sq-divider-hr" />
+      </div>
+
+      <fieldset className="sq-fieldset">
+        <CreditCardNumberInput />
+
+        <div className="sq-form-third">
+          <CreditCardExpirationDateInput />
+        </div>
+
+        <div className="sq-form-third">
+          <CreditCardPostalCodeInput />
+        </div>
+
+        <div className="sq-form-third">
+          <CreditCardCVVInput />
+        </div>
+      </fieldset>
+
+      <CreditCardSubmitButton>Pay $1.00</CreditCardSubmitButton>
+
+      <div className="sq-error-message">
+        {errorMessages.map(errorMessage => (
+          <li key={`sq-error-${errorMessage}`}>{errorMessage}</li>
+        ))}
+      </div>
+    </SquarePaymentForm>
+  );
+};
+
+export default SquarePurchase;

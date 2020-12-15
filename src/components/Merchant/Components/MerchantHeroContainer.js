dabb8a957ab3d759a944b9203ce46c0cdb7d7f23
Trenton Wesche <TrWesche@live.com>
2020-12-14 22:50:34 -0600

    Start adding hero tabs

--- src/components/Merchant/Components/MerchantHeroContainer.js
+++ src/components/Merchant/Components/MerchantHeroContainer.js
@@ -1,8 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import {
-    Grid
+    Grid,
+    Tabs,
+    Tab
 } from '@material-ui/core';
+import SwipeableViews from "react-swipeable-views";
 import FeaturedProductsStepper from "./FeaturedProductsStepper";
 import MerchantBiosStepper from "./MerchantBiosStepper";
 import MerchantGatheringsList from "./MerchantGatheringsList";
@@ -18,20 +21,38 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
+
+// TODO: Functional Tabs
 export default function MerchantHeroContainer({gatherings, featured_products, bios}) {
     const classes = useStyles();
+    const [value, setValue] = React.useState(0);
+
+    const handleChange = (event, newValue) => {
+      setValue(newValue);
+    };
 
     return (
         <Grid container className={classes.root}>
-            <Grid item xs={12}>
-                <FeaturedProductsStepper featuredProducts={featured_products} />
-            </Grid>
-            <Grid item xs={12}>
-                <MerchantBiosStepper merchantBios={bios} />
-            </Grid>
-            <Grid item xs={12}>
-                <MerchantGatheringsList gatherings={gatherings} />
-            </Grid>
+            <FeaturedProductsStepper featuredProducts={featured_products} />
+            <MerchantBiosStepper merchantBios={bios} />
+            <MerchantGatheringsList gatherings={gatherings} />
+            <Tabs
+                value={value}
+                onChange={handleChange}
+                indicatorColor="primary"
+                textColor="primary"
+                centered
+            >
+                <Tab label="Item One">
+                    
+                </Tab>
+                <Tab label="Item Two">
+                    
+                </Tab>
+                <Tab label="Item Three">
+                    
+                </Tab>
+            </Tabs>
         </Grid>
   );
 }
